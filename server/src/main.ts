7eb11b34107369227d18aa6d6e4c755d85108507
wx-hanxh3 <kanxh@strongdata.com.cn>
2026-08-03 10:41:17 +0800

    feat：update

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
     origin: true,
     credentials: true,
   });
+
+  app.useStaticAssets(join(__dirname, '..', '..', 'public'));
 
   app.useGlobalPipes(
     new ValidationPipe({
